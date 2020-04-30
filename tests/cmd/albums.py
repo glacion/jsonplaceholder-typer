22567a7b75e7d16fa9d62aac72ab188b4d4cc290
Ahmetcan Güvendiren <ahm@glacion.com>
2020-04-30 19:29:20 +0300

    Rename albums subcommand tests.

--- tests/cmd/albums.py
+++ tests/cmd/albums.py
@@ -21,37 +21,37 @@ def test_list():
     assert result.exit_code == 0
 
 
-def test_list_with_extra_args():
+def test_list_extra_args():
     result = runner.invoke(albums.app, ['list', 'asadf'])
     assert result.exit_code == 2
 
 
-def test_list_with_userid():
+def test_list_userid():
     result = runner.invoke(albums.app, ['list', '--userid', '1'])
     assert result.exit_code == 0
     assert len(result.output.splitlines()) == 11
 
 
-def test_list_with_out_of_range_userid():
+def test_list_out_of_range_userid():
     result = runner.invoke(albums.app, ['list', '--userid', '0'])
     assert result.exit_code == 2
     assert 'Error: Invalid value for \'--userid\':' in result.output
 
 
-def test_create_with_missing_userid():
+def test_create_missing_userid():
     result = runner.invoke(albums.app, ['create'])
     assert result.exit_code == 2
     assert 'Error: Missing option \'--userid\'' in result.output
 
 
-def test_create_with_out_of_range_userid():
+def test_create_out_of_range_userid():
     result = runner.invoke(
         albums.app, ['create', '--userid', '0', '--title', 'test'])
     assert result.exit_code == 2
     assert 'Error: Invalid value for \'--userid\':' in result.output
 
 
-def test_create_with_missing_title():
+def test_create_missing_title():
     result = runner.invoke(albums.app, ['create', '--userid', '123'])
     assert result.exit_code == 2
     assert 'Error: Missing option \'--title\'' in result.output
@@ -70,56 +70,56 @@ def test_get_without_id():
     assert 'Error: Missing argument \'ID\'.' in result.output
 
 
-def test_get_with_correct_id():
+def test_get_correct_id():
     result = runner.invoke(albums.app, ['get', '1'])
     assert result.exit_code == 0
     assert 'id=1, userId=1, title=quidem molestiae enim' in result.output
 
 
-def test_get_with_incorrect_id():
+def test_get_incorrect_id():
     result = runner.invoke(albums.app, ['get', '12345'])
     assert result.exit_code == 127
     assert 'Failure to retrieve resource.' in result.output
 
 
-def test_get_with_out_of_range_id():
+def test_get_out_of_range_id():
     result = runner.invoke(albums.app, ['get', '0'])
     assert result.exit_code == 2
     assert 'Error: Invalid value for \'ID\':' in result.output
 
 
-def test_update_with_incorrect_id():
+def test_update_incorrect_id():
     result = runner.invoke(albums.app, ['update', '12345'])
     assert result.exit_code == 127
     assert 'Failure to retrieve resource.' in result.output
 
 
-def test_update_with_correct_id_no_change():
+def test_update_no_change():
     result = runner.invoke(albums.app, ['update', '1'])
     assert result.exit_code == 0
     assert 'id=1, userId=1, title=quidem molestiae enim' in result.output
 
 
-def test_update_with_correct_id_title_change():
+def test_update_change_title():
     result = runner.invoke(albums.app, ['update', '1', '--title', 'test'])
     assert result.exit_code == 0
     assert 'id=1, userId=1, title=test' in result.output
 
 
-def test_update_with_correct_id_userid_change():
+def test_update_change_userid():
     result = runner.invoke(albums.app, ['update', '1', '--userid', '42'])
     assert result.exit_code == 0
     assert 'id=1, userId=42, title=quidem molestiae enim' in result.output
 
 
-def test_update_with_correct_id_both_change():
+def test_update_change_all():
     result = runner.invoke(
         albums.app, ['update', '1', '--title', 'test', '--userid', '42'])
     assert result.exit_code == 0
     assert 'id=1, userId=42, title=test' in result.output
 
 
-def test_update_with_out_of_range_id():
+def test_update_out_of_range_id():
     result = runner.invoke(albums.app, ['update', '0'])
     assert result.exit_code == 2
     assert 'Error: Invalid value for \'ID\':' in result.output
@@ -131,7 +131,7 @@ def test_delete():
     assert 'Resource deleted successfully.' in result.output
 
 
-def test_delete_with_out_of_range_id():
+def test_delete_out_of_range_id():
     result = runner.invoke(albums.app, ['delete', '0'])
     assert result.exit_code == 2
     assert 'Error: Invalid value for \'ID\':' in result.output

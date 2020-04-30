61e74ec5d36f004e7b8d79ad7e5c3ad8b4ae1c5e
Ahmetcan Güvendiren <ahm@glacion.com>
2020-04-30 03:01:43 +0300

    Fix module imports and add program description

--- jsonplaceholder/main.py
+++ jsonplaceholder/main.py
@@ -1,24 +1,25 @@
-import cmd.albums
-import cmd.comments
-import cmd.photos
-import cmd.posts
-import cmd.todos
-import cmd.users
-
+import jsonplaceholder.cmd.albums as albums
+import jsonplaceholder.cmd.comments as comments
+import jsonplaceholder.cmd.photos as photos
+import jsonplaceholder.cmd.posts as posts
+import jsonplaceholder.cmd.todos as todos
+import jsonplaceholder.cmd.users as users
 import typer
 
 app = typer.Typer()
-app.add_typer(cmd.albums.app, name='albums', help='Manage albums')
-app.add_typer(cmd.comments.app, name='comments', help='Manage comments')
-app.add_typer(cmd.photos.app, name='photos', help='Manage photos')
-app.add_typer(cmd.posts.app, name='posts', help='Manage posts')
-app.add_typer(cmd.todos.app, name='todos', help='Manage todos')
-app.add_typer(cmd.users.app, name='users', help='Manage users')
+app.add_typer(albums.app, name='albums', help='Manage albums')
+app.add_typer(comments.app, name='comments', help='Manage comments')
+app.add_typer(photos.app, name='photos', help='Manage photos')
+app.add_typer(posts.app, name='posts', help='Manage posts')
+app.add_typer(todos.app, name='todos', help='Manage todos')
+app.add_typer(users.app, name='users', help='Manage users')
 
 
 @app.callback()
 def main():
-    typer.echo(f"Main")
+    """
+    Command Line Interface for https://jsonplaceholder.typicode.com .
+    """
 
 
 if __name__ == "__main__":
